d9728704cf556b0e3eabcd141250213616b899d4
Sebastian Lackner <sebastian@fds-team.de>
2014-11-07 03:26:18 +0100

    ntdll: Return correct values in GetThreadTimes() for all threads.
    Based on a patch by Ray Hinchliffe <ray@pobox.co.uk>.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -1026,6 +1026,8 @@ struct get_thread_times_reply
     struct reply_header __header;
     timeout_t    creation_time;
     timeout_t    exit_time;
+    int          unix_pid;
+    int          unix_tid;
 };
 
 
@@ -6810,7 +6812,7 @@ union generic_reply
 
 /* ### protocol_version begin ### */
 
-#define SERVER_PROTOCOL_VERSION 603
+#define SERVER_PROTOCOL_VERSION 604
 
 /* ### protocol_version end ### */
 
