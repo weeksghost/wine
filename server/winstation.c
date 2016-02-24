cb12ac5b77d1cf8b58264f91a84904ee2ad26d0e
Sebastian Lackner <sebastian@fds-team.de>
2016-02-24 19:18:52 +0100

    server: Assign random name when no name was passed to create_winstation.

--- server/winstation.c
+++ server/winstation.c
@@ -38,6 +38,7 @@
 #include "user.h"
 #include "file.h"
 #include "security.h"
+#include "unicode.h"
 
 
 static struct list winstation_list = LIST_INIT(winstation_list);
@@ -114,9 +115,28 @@ static const struct object_ops desktop_ops =
 static struct winstation *create_winstation( struct object *root, const struct unicode_str *name,
                                              unsigned int attr, unsigned int flags )
 {
+    static unsigned int id = 0x10000;
     struct winstation *winstation;
+    struct unicode_str default_name;
+    char buffer[32];
 
-    if ((winstation = create_named_object( root, &winstation_ops, name, attr, NULL )))
+    if (name->len)
+    {
+        winstation = create_named_object( root, &winstation_ops, name, attr, NULL );
+        goto done;
+    }
+
+    do
+    {
+        if (!++id) id = 1;  /* avoid an id of 0 */
+        sprintf( buffer, "Service-0x0-%x$", id);
+        ascii_to_unicode_str( buffer, &default_name );
+        winstation = create_named_object( root, &winstation_ops, &default_name, attr & ~OBJ_OPENIF, NULL );
+    }
+    while (!winstation && get_error() == STATUS_OBJECT_NAME_COLLISION);
+
+done:
+    if (winstation)
     {
         if (get_error() != STATUS_OBJECT_NAME_EXISTS)
         {
@@ -134,6 +154,7 @@ static struct winstation *create_winstation( struct object *root, const struct u
         }
         else clear_error();
     }
+
     return winstation;
 }
 
