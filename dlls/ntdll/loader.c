2aedd2c34a7e4971cc6e53ccbc2312cabc9c9484
Derek Lesho <dlesho@codeweavers.com>
2020-07-13 13:04:16 -0500

    Set 0x4000 flag on LDR_MODULE, eac needs it.

--- dlls/ntdll/loader.c
+++ dlls/ntdll/loader.c
@@ -1238,7 +1238,7 @@ static WINE_MODREF *alloc_module( HMODULE hModule, const UNICODE_STRING *nt_name
 
     wm->ldr.BaseAddress   = hModule;
     wm->ldr.SizeOfImage   = nt->OptionalHeader.SizeOfImage;
-    wm->ldr.Flags         = LDR_DONT_RESOLVE_REFS | (builtin ? LDR_WINE_INTERNAL : 0);
+    wm->ldr.Flags         = 0x4000 | LDR_DONT_RESOLVE_REFS | (builtin ? LDR_WINE_INTERNAL : 0);
     wm->ldr.TlsIndex      = -1;
     wm->ldr.LoadCount     = 1;
 
